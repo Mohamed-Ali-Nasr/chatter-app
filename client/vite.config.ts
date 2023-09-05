816addca3716d8f212cdbd068f0490a2fc894827
Mohamed Ali Nasr <mohamedalinasr4444@gmail.com>
2023-09-05 21:59:09 +0300

    edit on vit.config.ts file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,9 @@ import { esbuildCommonjs } from "@originjs/vite-plugin-commonjs";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+
+  resolve: { mainFields: [] },
+
   optimizeDeps: {
     esbuildOptions: {
       plugins: [esbuildCommonjs(["react-moment"])],
