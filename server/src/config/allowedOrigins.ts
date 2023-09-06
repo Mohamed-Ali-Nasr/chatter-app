b5b1110a9213040877b7d38ba79553d15c4a8a5a
Mohamed Ali Nasr <mohamedalinasr4444@gmail.com>
2023-09-06 18:14:18 +0300

    add frontend url

--- server/src/config/allowedOrigins.ts
+++ server/src/config/allowedOrigins.ts
@@ -5,6 +5,6 @@ export const allowedOrigins = [
   "http://127.0.0.1:3000",
   "http://192.168.1.4:3000",
   "http://192.168.1.3:3000",
-  "https://chatter-app-frontend-3594.onrender.com/",
+  "https://chatter-app-frontend-3594.onrender.com",
   "http://localhost:5173",
 ];
