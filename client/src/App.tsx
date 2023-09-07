cd457998cbe5e859074962a4bd263fdf37038e4a
Mohamed Ali Nasr <mohamedalinasr4444@gmail.com>
2023-09-07 04:40:44 +0300

    edit on App.tsx file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import CustomToastContainer from "components/CustomToastContainer";
 import Layout from "components/Layout";
 import PersistLogin from "components/PersistLogin";
 import Prefetch from "components/Prefetch";
-import { AnimatePresence } from "framer-motion";
 import Chat from "pages/Chat";
 import Home from "pages/Home";
 import SignIn from "pages/SignIn";
@@ -13,41 +12,37 @@ import { Route, Routes } from "react-router-dom";
 const App = () => {
   return (
     <>
-      <AnimatePresence mode="wait">
-        <Routes>
-          <Route element={<Layout />}>
-            <Route element={<PersistLogin />}>
-              <Route path="/" id="home" index element={<Home />} />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route element={<PersistLogin />}>
+            <Route path="/" id="home" index element={<Home />} />
 
-              <Route
-                path="sign-in"
-                id="sign-in"
-                index
-                element={
-                  <Account>
-                    <SignIn />
-                  </Account>
-                }
-              />
+            <Route
+              path="sign-in"
+              id="sign-in"
+              element={
+                <Account>
+                  <SignIn />
+                </Account>
+              }
+            />
 
-              <Route
-                path="sign-up"
-                id="sign-up"
-                index
-                element={
-                  <Account>
-                    <SignUp />
-                  </Account>
-                }
-              />
+            <Route
+              path="sign-up"
+              id="sign-up"
+              element={
+                <Account>
+                  <SignUp />
+                </Account>
+              }
+            />
 
-              <Route element={<Prefetch />}>
-                <Route index id="chat" path="/chat" element={<Chat />} />
-              </Route>
+            <Route element={<Prefetch />}>
+              <Route id="chat" path="/chat" element={<Chat />} />
             </Route>
           </Route>
-        </Routes>
-      </AnimatePresence>
+        </Route>
+      </Routes>
 
       <CustomToastContainer />
     </>
